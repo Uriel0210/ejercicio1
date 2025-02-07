437ece88010596bf098ec7729c522da24f78955b
uriel martinez <uriel123mar123@gmail.com>
2025-02-07 12:10:00 -0600

    agregué el enunciado

--- src/Ejercicio16.js
+++ src/Ejercicio16.js
@@ -1,3 +1,7 @@
+/*Convertir cierta cantidad de minutos en horas y minutos, para la obtención de las horas utilizar división entera
+con el operador OR bit a bit ( | ) y para los minutos restantes utilizar el operador MODULO. Mostrar el
+resultado en consola utilizando Template String.*/
+
 let minutos = 123;
 function convercion(minutos) {
     let horas = minutos / 60 | 0;
